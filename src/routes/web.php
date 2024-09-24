c1d9843b8df3fe82529760c14332a0144c7d2d9d
Alexej Krzewitzki <alexej.k@ultrabold.com>
2024-09-24 20:42:22 +0200

    Fixed routes again

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,12 +1,7 @@
 <?php
 
-use Flobbos\PageComposer\Livewire\BugComponent;
-use Flobbos\PageComposer\Livewire\PageIndex;
-use Flobbos\PageComposer\Livewire\PageComposer;
-use Flobbos\PageComposer\Livewire\Frontend\PageDisplay;
-
 //Protected routes
-Route::group(['middleware' => config('pagecomposer.middleware'), 'namespace' => 'Flobbos\PageComposer\Livewire;', 'prefix' => 'page-composer', 'as' => 'page-composer::'], function () {
+Route::group(['middleware' => config('pagecomposer.middleware'), 'namespace' => 'Flobbos\PageComposer\Livewire', 'prefix' => 'page-composer', 'as' => 'page-composer::'], function () {
     Route::get('/', BugComponent::class)->name('dashboard');
     Route::get('pages', PageIndex::class)->name('pages.index');
     Route::get('pages/create', PageComposer::class)->name('pages.create');
