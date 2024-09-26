1477d93dec5dd7c21cc55e3af9e071a8185c414e
Alexej Krzewitzki <alexej.k@ultrabold.com>
2024-09-26 18:33:35 +0200

    Registered all components

--- src/PageComposer/PageComposerServiceProvider.php
+++ src/PageComposer/PageComposerServiceProvider.php
@@ -2,15 +2,49 @@
 
 namespace Flobbos\PageComposer;
 
+use Illuminate\Foundation\AliasLoader;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Validator;
-use Illuminate\Foundation\AliasLoader;
+use Flobbos\PageComposer\Livewire\BugComponent;
+use Flobbos\PageComposer\Livewire\CategoryComponent;
+use Flobbos\PageComposer\Livewire\ColumnComponent;
+use Flobbos\PageComposer\Livewire\CommentComponent;
+use Flobbos\PageComposer\Livewire\DatePicker;
+use Flobbos\PageComposer\Livewire\ElementComponent;
+use Flobbos\PageComposer\Livewire\ElementList;
+use Flobbos\PageComposer\Livewire\ImageUploadComponent;
+use Flobbos\PageComposer\Livewire\LanguageComponent;
+use Flobbos\PageComposer\Livewire\MultiSelect;
+use Flobbos\PageComposer\Livewire\MultiSelectInput;
+use Flobbos\PageComposer\Livewire\PageComposer;
+use Flobbos\PageComposer\Livewire\RowComponent;
+use Flobbos\PageComposer\Livewire\SelectInput;
+use Flobbos\PageComposer\Livewire\TagComponent;
+use Livewire\Livewire;
 
 class PageComposerServiceProvider extends ServiceProvider
 {
 
   public function boot()
   {
+    //Register components
+    Livewire::component('bug-component', BugComponent::class);
+    Livewire::component('category-component', CategoryComponent::class);
+    Livewire::component('column-component', ColumnComponent::class);
+    Livewire::component('comment-component', CommentComponent::class);
+    Livewire::component('element-component', ElementComponent::class);
+    Livewire::component('element-list', ElementList::class);
+    Livewire::component('date-picker', DatePicker::class);
+    Livewire::component('image-upload-component', ImageUploadComponent::class);
+    Livewire::component('language-component', LanguageComponent::class);
+    Livewire::component('multi-select', MultiSelect::class);
+    Livewire::component('multi-select-input', MultiSelectInput::class);
+    Livewire::component('page-composer', PageComposer::class);
+    Livewire::component('page-index', CategoryComponent::class);
+    Livewire::component('row-component', RowComponent::class);
+    Livewire::component('select-input', SelectInput::class);
+    Livewire::component('tag-component', TagComponent::class);
+
     //Publish config
     $this->publishes([
       __DIR__ . '/../config/pagecomposer.php' => config_path('pagecomposer.php'),
